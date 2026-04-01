4d54fd12d67a789c1a6eb6b218126f0dcf85b96d
Neeraj <neeraj.cseh@gmail.com>
2026-04-01 16:21:13 +0530

    add

--- ch1/ch1/urls.py
+++ ch1/ch1/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('myfun/', views.myfunction, name='myfunction'),
 ]
