e49c94d522531365adf3f67e37de497c25b8ad7c
Neeraj <neeraj.cseh@gmail.com>
2026-04-01 14:42:41 +0530

    add

--- ch1/ch1/urls.py
+++ ch1/ch1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sms project.
+URL configuration for ch1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
